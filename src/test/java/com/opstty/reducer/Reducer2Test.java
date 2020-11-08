dc46dae3a28c4bac529953d2026e3d51ff3f937a
Sarah <emmanuelle-sarah.oyono.bisso@efrei.net>
2020-11-08 20:06:56 +0100

    Question 2

---
+++ src/test/java/com/opstty/reducer/Reducer2Test.java
@@ -0,0 +1,37 @@
+package com.opstty.reducer;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
+public class Reducer2Test {
+    @Mock
+    private Reducer.Context context;
+    private Reducer2 reducer2;
+
+    @Before
+    public void setup() {
+        this.reducer2 = new Reducer2();
+    }
+
+    @Test
+    public void testReduce() throws IOException, InterruptedException {
+        String key = "key";
+        IntWritable value = new IntWritable(1);
+        Iterable<IntWritable> values = Arrays.asList(value, value, value);;
+        this.reducer2.reduce(new Text(key), values, this.context);
+        verify(this.context).write(new Text(key), NullWritable.get());
+    }
+}
