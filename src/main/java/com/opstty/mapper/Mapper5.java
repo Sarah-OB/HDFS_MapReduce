e3586e90a7f5e40fecada5f030d4f6f545f4b959
Sarah <emmanuelle-sarah.oyono.bisso@efrei.net>
2020-11-08 20:10:58 +0100

    Question 5

---
+++ src/main/java/com/opstty/mapper/Mapper5.java
@@ -0,0 +1,27 @@
+package com.opstty.mapper;
+
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import java.io.IOException;
+
+public class Mapper5 extends Mapper<Object, Text, Text, NullWritable> {
+    private Text height = new Text();
+
+    public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+        String csv = value.toString();
+        String[] arrLine = csv.split(";");
+
+        if (arrLine[0].equals("GEOPOINT") || arrLine[1].equals("ARRONDISSEMENT") || arrLine[2].equals("GENRE") || arrLine[3].equals("ESPECE") || arrLine[4].equals("FAMILLE") || arrLine[5].equals("ANNEE PLANTATION") || arrLine[6].equals("HAUTEUR") || arrLine[7].equals("CIRCONFERENCE") || arrLine[8].equals("ADRESSE") || arrLine[9].equals("NOM COMMUN") || arrLine[10].equals("VARIETE") || arrLine[11].equals("OBJECTID") || arrLine[12].equals("NOM_EV")){
+            return;
+        }
+
+        height.set(arrLine[6]);
+
+        if (!(height.toString().equals("\t") || height.toString().equals(" ") || height.toString().equals(""))) {
+            context.write(height, NullWritable.get());
+        }
+    }
+
+}
+
